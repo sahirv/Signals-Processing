c87ab55688e5cc340d653afed9cfd9f7bb830d5a
sahir <sahir.vellani@gmail.com>
2016-12-29 22:28:54 -0500

    Upload new convolution.m

--- Convolution.m
+++ Convolution.m
@@ -1,37 +1,38 @@
-clear;
+%inputs are sampling frequency, function x (with time period included,
+%and function y (with time period included)
+%For example: AQ1b(100,sin(0:0.01:2*pi),2*cos(0:0.01:4*pi))
+%For example: AQ1b(10,ones(1,100),ones(1,10))
 
-%test inputs
-input1 = 0.01:0.01:2;
-input2 = ones(1,100);
-dT = 0.01;
-
-%variables x and y are assigned
-if (size(input1, 2) > size(input2, 2))
-    x = input1;
-    y = input2;
-else
-    x = input2;
-    y = input1;
-end
-
-z = [];
-
-%flips the array over as should be done in convolution y(k) --> y(-k)
-y = y(end:-1:1);
-
-%for loop to shift signal y from 1 to len(y) + len(x) + 2
-for n = 1:((size(y,2)+size(x,2)+2))
-    sum = 0;
-    m = 0;
-    for m = 1:size(y,2)
-        %if overlap occurs, sum all the products, if not, add 0
-        if ((n-m) > 0 && n-m < (size(x,2)+1))
-            sum = sum + x(1,n-m)*y(1,size(y,2)- m + 1)*dT;
-        else
-            sum = sum + 0;
-        end
+function z = AQ1b(f, input1, input2) 
+    if (size(input1, 2) > size(input2, 2))
+        x = input1;
+        y = input2;
+    else
+        x = input2;
+        y = input1;
     end
-    z = [z sum];
+    
+    dT =1/f;
+    z = [];
+    y = y(end:-1:1);
+    
+    for n = 1:((size(y,2)+size(x,2)+2))
+        sum = 0;
+        m = 0;
+        for m = 1:size(y,2)
+            if ((n-m) > 0 && n-m < (size(x,2)+1))
+                sum = sum + x(1,n-m)*y(1,size(y,2)- m + 1)*dT;
+            else
+                sum = sum + 0;
+            end
+        end
+        z = [z sum];
+    end
+    figure, plot(z)
 end
 
-figure, plot(z)
+
+
+
+
+
